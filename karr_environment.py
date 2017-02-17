68567f4b443ca36e67f18f01995b38f05f33b6a0
John Lackey <john.lackey@jefferson.edu>
2017-02-17 15:26:23 -0500

    daily

--- karr_environment.py
+++ karr_environment.py
@@ -46,7 +46,7 @@ class KarrEnvironment(Environment, Named):
         self.car = Car.Car()
 
     def command(self, action):
-        speed = 150
+        speed = 100
         print(action)
         if action == "q":
             self.car.forward(speed)
