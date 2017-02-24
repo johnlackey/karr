40584fc940878c756570d33a339dd433a796036b
John Lackey <john.lackey@jefferson.edu>
2017-02-24 12:58:45 -0500

    gpiozero DistanceSensor

--- Car.py
+++ Car.py
@@ -7,7 +7,8 @@ import array
 import atexit
 import time
 import RPi.GPIO as GPIO
-#import statistics
+from gpiozero import DistanceSensor
+# import statistics
 
 from Adafruit_MotorHAT import Adafruit_MotorHAT
 
@@ -40,19 +41,23 @@ class Car(object):
         # Configure all motors to stop at program exit if desired.
         if stop_at_exit:
             atexit.register(self.stop)
-	# setup ultrasonic sensor
-        self.TRIG = [11, 15, 18]
-        self.ECHO = [12, 16, 22]
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.TRIG[0], GPIO.OUT)
-        GPIO.setup(self.ECHO[0], GPIO.IN)
-        GPIO.setup(self.TRIG[1], GPIO.OUT)
-        GPIO.setup(self.ECHO[1], GPIO.IN)
-        GPIO.setup(self.TRIG[2], GPIO.OUT)
-        GPIO.setup(self.ECHO[2], GPIO.IN)
+        # setup ultrasonic sensor
+        # self.TRIG = [11, 15, 18]
+        # self.ECHO = [12, 16, 22]
+        # GPIO.setmode(GPIO.BOARD)
+        # GPIO.setup(self.TRIG[0], GPIO.OUT)
+        # GPIO.setup(self.ECHO[0], GPIO.IN)
+        # GPIO.setup(self.TRIG[1], GPIO.OUT)
+        # GPIO.setup(self.ECHO[1], GPIO.IN)
+        # GPIO.setup(self.TRIG[2], GPIO.OUT)
+        # GPIO.setup(self.ECHO[2], GPIO.IN)
         #
-        #https://www.sunfounder.com/learn/sensor-kit-v2-0-for-raspberry-pi-b-plus/lesson-25-ultrasonic-ranging-module-sensor-kit-v2-0-for-b-plus.html
-
+        self.sensor = [
+            DistanceSensor(17, 18),
+            DistanceSensor(22, 23),
+            DistanceSensor(24, 25)
+        ]
+        # https://www.sunfounder.com/learn/sensor-kit-v2-0-for-raspberry-pi-b-plus/lesson-25-ultrasonic-ranging-module-sensor-kit-v2-0-for-b-plus.html
 
     def _drive_speed(self, speed):
         """Set the speed of the drive motor, taking into account its trim offset.
@@ -82,9 +87,9 @@ class Car(object):
         """
         # Set motor speed and move both forward.
         self._drive_speed(speed)
-        #self._steer_speed(speed)
+        # self._steer_speed(speed)
         self._drive.run(Adafruit_MotorHAT.FORWARD)
-        #self._steer.run(Adafruit_MotorHAT.RELEASE)
+        # self._steer.run(Adafruit_MotorHAT.RELEASE)
         # If an amount of time is specified, move for that time and then stop.
         if seconds is not None:
             time.sleep(seconds)
@@ -97,9 +102,9 @@ class Car(object):
         """
         # Set motor speed and move both backward.
         self._drive_speed(speed)
-        #self._steer_speed(speed)
+        # self._steer_speed(speed)
         self._drive.run(Adafruit_MotorHAT.BACKWARD)
-        #self._steer.run(Adafruit_MotorHAT.RELEASE)
+        # self._steer.run(Adafruit_MotorHAT.RELEASE)
         # If an amount of time is specified, move for that time and then stop.
         if seconds is not None:
             time.sleep(seconds)
@@ -114,10 +119,10 @@ class Car(object):
         spin for that amount of time and then stop.
         """
         # Set motor speed and move both forward.
-        #self._drive_speed(speed)
+        # self._drive_speed(speed)
         self._steer_speed(255)
         self._steer.run(Adafruit_MotorHAT.FORWARD)
-        #self._drive.run(Adafruit_MotorHAT.FORWARD)
+        # self._drive.run(Adafruit_MotorHAT.FORWARD)
         # If an amount of time is specified, move for that time and then stop.
         if seconds is not None:
             time.sleep(seconds)
@@ -129,25 +134,25 @@ class Car(object):
         spin for that amount of time and then stop.
         """
         # Set motor speed and move both forward.
-        #self._drive_speed(speed)
+        # self._drive_speed(speed)
         self._steer_speed(255)
         self._steer.run(Adafruit_MotorHAT.BACKWARD)
-        #self._drive.run(Adafruit_MotorHAT.FORWARD)
+        # self._drive.run(Adafruit_MotorHAT.FORWARD)
         # If an amount of time is specified, move for that time and then stop.
         if seconds is not None:
             time.sleep(seconds)
             self.stop()
 
-    def raw_distance(self, sensor = 0):
+    def raw_distance(self, sensor=0):
         if GPIO.input(self.ECHO[sensor]):
-            return(100)
+            return (100)
         distance = 0
         GPIO.output(self.TRIG[sensor], 0)
         time.sleep(0.05)
         GPIO.output(self.TRIG[sensor], 1)
         dummy_variable = 0
         dummy_variable = 0
-        #time.sleep(0.00001)
+        # time.sleep(0.00001)
         GPIO.output(self.TRIG[sensor], 0)
         time1, time2 = time.time(), time.time()
         while GPIO.input(self.ECHO[sensor]) == 0:
@@ -165,32 +170,32 @@ class Car(object):
                 distance = 500
                 break
         if distance == 500:
-            return(distance)
+            return (distance)
         during = time2 - time1
         distance_value = during / 0.00000295 / 2 / 10
-        #print( "Raw Distance: %d" % distance_value )
+        # print( "Raw Distance: %d" % distance_value )
         return distance_value
 
-    def distance(self, sensor = 0):
-	#sum = 0.0
-	list = []
-	for i in range(1):
-            #sum += self.raw_distance(sensor)
+    def distance(self, sensor=0):
+        # sum = 0.0
+        list = []
+        for i in range(1):
+            # sum += self.raw_distance(sensor)
             value = 100
             while value == 100:
-                value = self.raw_distance(sensor)
-	    if value != 500:
+                # value = self.raw_distance(sensor)
+                value = self.sensor[sensor].distance * 100
+            if value != 500:
                 list.append(value)
         list.sort()
-	#print( "Length of list: %d" % len(list) )
+        # print( "Length of list: %d" % len(list) )
         if len(list) > 0:
             index = len(list) // 2
-            #print( "Index: %d" % index )
-            print( "Distance%d: %d" % (sensor, list[len(list)//2]) )
+            # print( "Index: %d" % index )
+            print("Distance%d: %d" % (sensor, list[len(list) // 2]))
             return list[index]
         return 9999
-        #return sum / 10
+        # return sum / 10
 
-    def destroy():
+    def destroy(self):
         GPIO.cleanup()
-
